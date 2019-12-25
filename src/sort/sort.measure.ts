fe88fc2303b71d337e26a48dd51ee33c1437aa44
yuanhjty <yuanhjty@163.com>
2019-12-25 15:49:08 +0800

    measure sort performance

---
+++ src/sort/sort.measure.ts
@@ -0,0 +1,6 @@
+import measureSort from './utils/measure-sort';
+import quickSortV1 from './quick-sort';
+import quickSortV2 from './quick-sort-v2';
+
+measureSort(quickSortV1);
+measureSort(quickSortV2);
---
+++ src/sort/utils/measure-sort.ts
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+/* eslint-disable no-param-reassign */
+import { Sort } from '../types';
+import getRandomList from './random-list';
+
+export default function measureSort(
+  sort: Sort<number>,
+  listSize?: number,
+  listCount?: number,
+): number {
+  listSize = listSize || 2000;
+  listCount = listCount || 2000;
+  const lists = [];
+  for (let i = 0; i < listCount; i++) {
+    lists.push(getRandomList(listSize));
+  }
+
+  const startTime = Date.now();
+  lists.forEach(list => sort(list, (a, b) => a - b));
+  const timeRange = Date.now() - startTime;
+  console.log(`sort ${listCount} lists of size ${listSize}: ${sort.name}: ${timeRange}ms`);
+  return timeRange;
+}
