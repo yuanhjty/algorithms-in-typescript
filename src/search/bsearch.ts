c501562e83dacfa24eaaee777b118498603181c3
shaozhiyuan <yuanhjty@163.com>
2020-01-03 10:40:46 +0800

    rename variable

--- src/search/bsearch.ts
+++ src/search/bsearch.ts
@@ -1,13 +1,13 @@
-export default function bsearch<T>(items: T[], item: T): number {
+export default function bsearch<T>(items: T[], target: T): number {
   let low = 0;
   let high = items.length - 1;
 
   while (low <= high) {
     const mid = low + Math.floor((high - low) / 2);
-    if (items[mid] === item) {
+    if (items[mid] === target) {
       return mid;
     }
-    if (items[mid] < item) {
+    if (items[mid] < target) {
       low = mid + 1;
       continue;
     }
